f61150bab8468b566e7353a95dfc1d7127b9ae84
Matt Brown <mattbrown600@gmail.com>
2016-06-28 02:10:29 -0700

    added in new parts of animation solution. added in untracked files that are needed to work remote

---
+++ Assets/Scripts/OrientationAngles.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+public class OrientationAngles : MonoBehaviour {
+
+    private Queue<float[]> angles = new Queue<float[]>();
+    private bool show = false;
+    private bool co = false;
+
+    void Update()
+    {
+        if (show)
+        {
+            if (!co)
+            {
+                StartCoroutine(showAngles());
+            }
+        }
+    }
+
+    public void addAngles(float[] latest)
+    {
+        lock (angles)
+        {
+            angles.Enqueue(latest);
+        }
+    }
+
+    public void getAngles()
+    {
+
+        StringBuilder result = new StringBuilder();
+        foreach (float a in angles.Dequeue()){
+            result.Append(a);
+            result.Append(",");
+        }
+
+        Debug.Log(result.ToString());
+    }
+
+    public void startShowingAngles()
+    {
+        show = true;      
+    }
+
+    public void stopShowing()
+    {
+        show = false;
+    }
+
+    private IEnumerator showAngles()
+    {
+        co = true;
+       
+            lock (angles)
+            {
+                getAngles();
+            }
+
+            Debug.Log("HEY");
+            
+            yield return null;
+       
+
+        co = false;
+    }
+	
+}
